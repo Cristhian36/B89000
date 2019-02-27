753c464f8f064f699485715cd506db63da6d797c
Candido <candidoacevedomedrano@gmail.com>
2019-02-26 20:58:23 -0400

    Contacto
    Diseño de contacto terminado.

--- vista/contacto.php
+++ vista/contacto.php
@@ -5,20 +5,30 @@
     <title>Contacto</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/kite-menu.css" />
+    <link rel="stylesheet" href="assets\css\contacto.css">
+
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.kitemenu.js"></script>
   </head>
   <body>
-    <form class="" action="email.php" method="post">
-      <input type="text" name="nombre" value="">
-      <input type="text" name="correo" value="">
-      <input type="text" name="telefono" value="">
-      <input type="text" name="mensaje" value="">
-      <button type="submit" class="btn btn-success" name="button">Enviar</button>
-    </form>
+    <?php
+      include_once 'ModulosHomePage/cabecera.php';
+      include_once 'ModulosHomePage/contenidoContacto.php';
+      include_once 'ModulosHomePage/footer.php';
+    ?>
+
+
+    <script>
+       $(".kite-menu").kiteMenu({
+          kiteSkin: "rainbow",
+          });
+    </script>
   </body>
 </html>
