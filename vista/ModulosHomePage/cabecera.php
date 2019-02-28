968b807b850ca6eb1c5f9c70d46707e7722b5c57
KelvinEncarnacion001 <35616333+KelvinEncarnacion001@users.noreply.github.com>
2019-02-28 16:02:02 -0400

    Revert "Revert "Revert "Diseño v1.0"""
    This reverts commit 4ee216e825c60a95a7816f3d15a0b5ecc8c50458.

--- vista/ModulosHomePage/cabecera.php
+++ vista/ModulosHomePage/cabecera.php
@@ -6,10 +6,10 @@
    </div>
    <div class="links-wrapper">
       <ul class="menu-links">
-         <li> <a href="#1">Home </a> </li>
-         <li> <a href="vista/contacto.php"> Blog</a> </li>
+         <li> <a href="vista/HomePage.php">Home</a> </li>
+         <li> <a href="vista/solicitudPrestamo.php">Solicitud Préstamos</a> </li>
          <li> <a href="#3">News </a> </li>
-         <li> <a href="#4">Contact </a> </li>
+         <li> <a href="vista/contacto.php">Contactos</a> </li>
          <li> <a href="#5">About </a> </li>
       </ul>
    </div>
