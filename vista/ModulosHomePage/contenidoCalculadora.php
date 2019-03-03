250fb9009e3b7047a623bc33ec77a7c6b7ccc59f
Marvyn <marvynharry@gmail.com>
2019-03-03 00:07:03 -0400

    calculadora

--- vista/ModulosHomePage/contenidoCalculadora.php
+++ vista/ModulosHomePage/contenidoCalculadora.php
@@ -1,6 +1,4 @@
 <script>
-// This script is explained line by line in depth in the following video:
-// http://www.developphp.com/view.php?tid=1389
 function computeLoan(){
 	var amount = document.getElementById('amount').value;
 	var interest_rate = document.getElementById('interest_rate').value;
@@ -13,7 +11,7 @@ function computeLoan(){
 </script>
 <body>
 <hr>
-<p>Cantidad a solicitar: $<input id="amount" type="number" min="1" max="1000000" onchange="computeLoan()"></p>
+<p>Cantidad a solicitar: $ <input id="amount" type="number" min="1" max="1000000" onchange="computeLoan()"></p>
 <p>Tasa de Interes: <input id="interest_rate" type="number" min="0" max="100" value="10" step=".1" onchange="computeLoan()">%</p>
 <p>Meses a pagar: <input id="months" type="number" min="1" max="72" value="1" step="1" onchange="computeLoan()"></p>
 <h2 id="payment"></h2>
